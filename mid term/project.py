fc64434c7fb2a51c6ae6b4da81528b88c8acf56c
group8BCS1 <group8progbcs@gmail.com>
2021-05-04 13:15:49 +0300

    FINAL VERSION OF THE PROGRAM

--- mid term/project.py
+++ mid term/project.py
@@ -1,5 +1,7 @@
 # A program to  be used by the utility company for billing its customers
+print("==================================================RESTART=======================================================")
 # function to calculate the gallons of water used
+
 
 def gallons(meter_reading1, meter_reading2):
     if meter_reading2 > meter_reading1:
@@ -21,17 +23,17 @@ def amount_billed():
     if code == "r":
         billed = 5.00 + (gallon * 0.005)
         billed = round(billed, 2)
-        print("Dear customer your bill is :", "$", billed)
+        print(f"Amount billed:${billed}")
     elif code == 'c':
         if gallon >= 4000000:
             billed = 1000.00 + (gallon - 4000000) * 0.00025
             billed = round(billed, 2)
-            print("Dear customer your bill is:", "$", billed)
+            print(f"Amount billed:${billed}")
     else:
         if 4000000 <= gallon <= 10000000:
             billed = 2000.00 + (gallon - 10000000) * 0.00025
             billed = round(billed, 2)
-            print("Dear customer your bill is:", "$", bill)
+            print(f"Amount billed:${billed}")
     return billed
 
 
@@ -46,24 +48,24 @@ while True:
     if 0 < beg_meter_reading < 999999999 and 0 < end_meter_reading < 999999999:
         if customer_code == "r":
             print("Customer code:", customer_code)
-            print(f"Enter beginning meter reading:{beg_meter_reading:09}")
-            print(f"Enter ending meter reading:{ end_meter_reading:09}")
+            print(f"Beginning meter reading:{beg_meter_reading:09d}")
+            print(f"Ending meter reading:{ end_meter_reading:09d}")
             gallon = gallons(beg_meter_reading, end_meter_reading)
             bill = amount_billed()
             gallon
             bill
         elif customer_code == "c":
             print("Customer code:", customer_code)
-            print(f"Enter beginning meter reading:{beg_meter_reading:09}")
-            print(f"Enter ending meter reading:{ end_meter_reading:09}")
+            print(f"Beginning meter reading:{beg_meter_reading:09d}")
+            print(f"Ending meter reading:{ end_meter_reading:09d}")
             gallon = gallons(beg_meter_reading, end_meter_reading)
             bill = amount_billed()
             gallon
             bill
         elif customer_code == " i ":
             print("Customer code:", customer_code)
-            print(f"Enter beginning meter reading:{beg_meter_reading:09}")
-            print(f"Enter ending meter reading:{ end_meter_reading:09}")
+            print(f"Beginning meter reading:{beg_meter_reading:09d}")
+            print(f"Ending meter reading:{ end_meter_reading:09d}")
             gallon = gallons(beg_meter_reading, end_meter_reading)
             bill = amount_billed()
             gallon
