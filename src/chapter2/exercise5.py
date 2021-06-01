ee09b673d5f0c905966b212e9b836911cb55e0f9
group8BCS1 <group8progbcs@gmail.com>
2021-06-01 16:11:55 +0300

    assignment done

--- src/chapter2/exercise5.py
+++ src/chapter2/exercise5.py
@@ -1,3 +1,3 @@
 temperature_celsius = input("Provide your temperature in degrees celsius \n")
-temperature_fahrenheit = (int(temperature_celsius) * 9/2) + 32
+temperature_fahrenheit = (int(temperature_celsius) * 9/5) + 32
 print("Your temperature in  fahrenheit is \n", temperature_fahrenheit)
